f9aa809b448121cb8c7c6550d06c2ba426743032
SinghProbjot <p.singh3@campus.unimib.it>
2026-01-13 21:11:27 +0100

    Initial Commit

---
+++ Synapse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Synapse
+//
+//  Created by Probjot Singh on 13/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Synapse/SynapseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SynapseApp.swift
+//  Synapse
+//
+//  Created by Probjot Singh on 13/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SynapseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
